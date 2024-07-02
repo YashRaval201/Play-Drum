e598181162d1533e93f73b3975695979abe3888b
Yash Raval <70638712+YashRaval201@users.noreply.github.com>
2024-07-03 00:46:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+var drumNumbers = document.querySelectorAll(".drum").length;
+
+for(var i = 0; i < drumNumbers; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+        var buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
+    });
+}
+
+document.addEventListener("keypress", function(event) {
+    // console.log(event);
+    makeSound(event.key);
+})
+
+function makeSound(key) {
+    switch (key) {
+        case "d":            
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();           
+            break;
+        
+        case "f":
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();           
+            break;
+
+        case "g":            
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();           
+            break;
+
+        case "h":            
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();           
+            break;
+
+        case "c":            
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();           
+            break;    
+
+        case "v":            
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();           
+            break;     
+
+        case "k":            
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();           
+            break;
+
+        case "b":            
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();           
+            break;
+
+        default:
+            break;
+    }
+}
+
